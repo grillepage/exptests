96f1650eb14556c9811f9ef8b3702c88051c02b2
unknown <forgeta04@gmail.com>
2020-08-07 18:02:07 +0300

    add test

--- cypress/integration/exampleTest.js
+++ cypress/integration/exampleTest.js
@@ -1,20 +1,34 @@
 /// <reference types="cypress" />
 
+describe('Пробный тест главной страницы', () => {
 
-describe('testcae', () => {
-    it ('Visits the TravelAsk', () => {
-        cy.fixture ('example').then(data => {
+    it ('Тест кейс 1', () => {
+        cy.fixture ('exampleFixtures').then(data => {
             cy.log('Переход на главную страницу')
             cy.visit(data.main_url)
         
-            cy.log('Печатаем текст в строке ввода')
+            cy.log('Скролл в в подвал сайта')               
+            cy.scrollTo('bottom',  { duration: 3000 })
+    
+            cy.log('Скролл обратно на первый экран')
+            cy.scrollTo('top', { duration: 3000 })
+    
+        })
+    })
+    it ('Тест кейс 2', () => {              
+        cy.fixture ('exampleFixtures').then(data => {
+            cy.log('В поле Input “Куда” напечатать текст')
             cy.get('input').click()
             .type(data.type_text)
 
-            cy.log('Нажимаем кнопку поиск')
+            cy.log('Нажать кнопку поиск')
             cy.get(data.button_search)
-            .click()        
+            .click()            
+        }) 
 
+    })    
+    it ('Тест кейс 3', () => {
+        cy.fixture ('exampleFixtures').then(data => {
             cy.log('возвращаемся обратно на главную страницу')
             cy.visit(data.main_url)
 
@@ -28,15 +42,16 @@ describe('testcae', () => {
 
             ///временное решение, так как на сайте баг. надо подумать в сторону проверки на наличие в dropdown списке 
             ///слов Париж и прочее
-            cy.wait(3000)
+            cy.wait(1000)
             
             cy.log('Ищем Париж')
             cy.get(data.button_search)
-            .click()    
+            .click()             
     
         })
-    })
-})    
+    })    
+})
+  
 
 
 
