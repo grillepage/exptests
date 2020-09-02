69420b48bd80d17610952a1a04625eaa52663363
unknown <forgeta04@gmail.com>
2020-09-02 17:14:08 +0300

    create simple tests

---
+++ cypress/integration/commercialSeoPages/commercial_seo_allTransport.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('Тест seo страницы: Весь транспорт', () => {
+    it ('Test 1', () => {
+       cy.fixture('commercialSeoPagesFixtures/commercial_seo_allTransport.json').then (data => {
+
+           cy.log('Переход на главну страницу')
+           cy.visit(data.main_url)
+           cy.log('Проверка на наличие поисковой формы в DOM')
+           cy.get(data.search_form)
+           .should('exist')
+         })
+    })
+})
---
+++ cypress/integration/commercialSeoPages/commercial_seo_railway.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('Тест seo страницы: Ж/Д Билеты', () => {
+    it ('Test 1', () => {
+       cy.fixture('commercialSeoPagesFixtures/commercial_seo_railway.json').then (data => {
+
+           cy.log('Переход на главну страницу')
+           cy.visit(data.main_url)
+           cy.log('Проверка на наличие поисковой формы в DOM')
+           cy.get(data.search_form)
+           .should('exist')
+         })
+    })
+})
