10038f5302ea448b812872b7b8e5c190639794dd
Alexander Bondarenko <grillepage@gmail.com>
2020-10-09 17:24:17 +0300

    Search test mod1

--- cypress/integration/commercialSearchForms/main_avia_search.spec.js
+++ cypress/integration/commercialSearchForms/main_avia_search.spec.js
@@ -8,10 +8,16 @@ describe('Тест работы коммерческой поисковой фо
             cy.get(info.button).contains(info.button_text).click()
             cy.log('Проверка на наличие поисковой формы в DOM')
             cy.get(info.form).should('be.visible')
-           
+            cy.log('Ввод запросов в поисковые поля')
+            cy.get(info.from_form).contains(info.from_form_text).click().type(info.from_form_type)
+            .wait(2000).get(info.air_choose).contains(info.air_1).click()
+            cy.get(info.to_form).contains(info.to_form_text).click().type(info.to_form_type)
+            .wait(2000).get(info.air_choose).contains(info.air_2).click()
+            cy.log('Переход к полям выбора даты')
+            cy.get('[class="datepicker avia-datepicker"]').should('be.visible')
+            cy.get('class="datepicker--cell datepicker--cell-day -current-"').should('be.visible')
+            
+
         })
     })
-    
-
-
 })
